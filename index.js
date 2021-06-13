1e1ef9fa492a730b8dda3d2efdb86da6ff0efaa2
lybell <futireflugel@gmail.com>
2021-06-13 18:14:14 +0900

    heroku!

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ io.on('connection', function(socket){
 
 
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 http.listen(port, function(){ 
   console.log('server on! http://localhost:'+port);
 });
